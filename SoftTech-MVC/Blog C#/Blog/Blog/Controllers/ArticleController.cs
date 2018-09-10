545bade16c6c9823eb819e36908afd9d41495e54
GeorgeNikolov <gnikolov1729@gmail.com>
2018-09-10 13:00:58 +0300

    All the Blogs

---
+++ SoftTech-MVC/Blog C#/Blog/Blog/Controllers/ArticleController.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Blog.Data;
+using Blog.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Blog.Controllers
+{
+    public class ArticleController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ArticleController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Article
+        public ActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        //GET: Article/List
+        public ActionResult List()
+        {
+            var articles = _context.Articles.Include(a => a.Author).ToList();
+            return View(articles);
+        }
+        // GET: Article/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var article = await _context.Articles
+                .Include(a => a.Author)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (article == null)
+            {
+                return NotFound();
+            }
+
+            return View(article);
+        }
+        
+        public bool IsUserAuthorizedToEdit(Article article)
+        {
+            return article.Author.UserName == this.User.Identity.Name ? true:false;
+        }
+
+        // GET: Article/Create
+        [Authorize]
+        public IActionResult Create()
+        {
+            ViewData["AuthorId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Article/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> Create([Bind("Id,Title,Content,AuthorId")] Article article)
+        {
+            if (ModelState.IsValid)
+            {
+                //get the user Id
+                var authorId = _context.Users.Where(u => u.UserName == this.User.Identity.Name)
+                    .First().Id;
+
+                //set article author 
+                article.AuthorId = authorId;
+
+                //save article
+                _context.Articles.Add(article);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewData["AuthorId"] = new SelectList(_context.Users, "Id", "Id", article.AuthorId);
+            return View(article);
+        }
+
+        // GET: Article/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var article = _context.Articles.Where(a => a.Id == id).First();
+            if (IsUserAuthorizedToEdit(article) == false)
+            {
+                return Forbid();
+            }
+            if (article == null)
+            {
+                return NotFound();
+            }
+
+           
+
+            ViewData["AuthorId"] = new SelectList(_context.Users, "Id", "Id", article.AuthorId);
+            return View(article);
+        }
+
+        // POST: Article/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Content,AuthorId")] Article article)
+        {
+            if (id != article.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(article);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ArticleExists(article.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AuthorId"] = new SelectList(_context.Users, "Id", "Id", article.AuthorId);
+            return View(article);
+        }
+
+        // GET: Article/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var article = await _context.Articles
+                .Include(a => a.Author)
+                .SingleOrDefaultAsync(m => m.Id == id);
+
+            if (IsUserAuthorizedToEdit(article) == false)
+            {
+                return Forbid();
+            }
+            if (article == null)
+            {
+                return NotFound();
+            }
+
+            return View(article);
+        }
+
+        // POST: Article/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var article = await _context.Articles.SingleOrDefaultAsync(m => m.Id == id);
+           
+            _context.Articles.Remove(article);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ArticleExists(int id)
+        {
+            return _context.Articles.Any(e => e.Id == id);
+        }
+    }
+}
